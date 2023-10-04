465d3075e4297ded39db00dbeb4eae91a7a63e47
Rahmani-Abdelhak <146826198+Rahmani-Abdelhak@users.noreply.github.com>
2023-10-04 15:19:15 +0100

    Update check.php
    Signed-off-by: Rahmani-Abdelhak <146826198+Rahmani-Abdelhak@users.noreply.github.com>

--- check.php
+++ check.php
@@ -2,7 +2,8 @@
 
 	if($_POST["user"] == "er@er.er" && $_POST["pass"] == "er159"){
 	
-		header('Location: http://elecreseaux.no-ip.org:8088/');
+		//header('Location: http://elecreseaux.no-ip.org:8088/');
+		header('Location: http://www.google.com');
 	
 	}
 
