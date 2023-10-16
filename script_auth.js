dc3f2b8ce5feffcc887383be2014ba473e791d73
Rahmani-Abdelhak <146826198+Rahmani-Abdelhak@users.noreply.github.com>
2023-10-16 12:56:58 +0100

    Add files via upload
    Signed-off-by: Rahmani-Abdelhak <146826198+Rahmani-Abdelhak@users.noreply.github.com>

---
+++ script_auth.js
@@ -0,0 +1,12 @@
+function connectUser() {
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var connectedUser = email + ":" + password
+    var users = ["test@test.com:test", "admin@admin.com:admin","user@email.com:1234"]
+    if (users.includes(connectedUser)) {
+        window.location.replace("https://github.com/louhab/repos2")
+    } else {
+        var alertDiv = document.querySelector('.alert-danger');
+        alertDiv.style.display = 'block';
+    }   
+}
